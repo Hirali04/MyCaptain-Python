07ad243fed745d30b85e0dc102233d5c637108ed
Hirali04 <130475886+Hirali04@users.noreply.github.com>
2023-04-21 20:25:17 +0530

    Add files via upload

---
+++ assgn_4.py
@@ -0,0 +1,11 @@
+list1 = [12,-7,5,64,-14]
+for num in list1:
+    if num>=0:
+        print(num, end =" ")
+print()
+list2 = [12,14,-95,3]
+print('[')
+for n in list2:
+    if n>=0:
+        print(n, end=" ")
+print(']')
